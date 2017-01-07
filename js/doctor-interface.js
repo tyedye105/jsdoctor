2e0bd5c978cc60ce0868176c3bd6482bb4820fb1
Marty <tyedye105@Martys-MacBook-Pro.local>
2017-01-06 16:20:54 -0800

    will finally show the doctors. oi...

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,15 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
-
 $(document).ready(function() {
   var foundDoctors = new Doctor();
+  allDoctors = [];
   $("#ailmentForm").submit(function(event){
     event.preventDefault();
     var ailment = $('#ailmentInput').val();
     foundDoctors.getDoctors(ailment);
   });
-
+  $("#show").click(function(event){
+    event.preventDefault();
+    $("=")
+    foundDoctors.displayDoctors();
+  })
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,16 +1,12 @@
 var apiKey = require('./../.env').apiKey;
-var allDoctors = [];
+allDoctors = []
 function Doctor(firstName, lastName, title) {
-  this.listDoctors = allDoctors;
-  this.FirstName = lastName;
-  this.LastName = firstName;
+  this.firstName = lastName;
+  this.lastName = firstName;
   this.title = title;
 }
 
-Doctor.prototype.displayDoctors = function () {
-$("#doctorOutput").html("dasdfaf");
-console.log(this.listDoctors.length)
-};
+
 
 
 
@@ -18,21 +14,28 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
   allDoctors = []
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(results) {
-     console.log(results);
      results.data.forEach(function(docData){
        var newDoctor = new Doctor(docData.profile.first_name, docData.profile.last_name, docData.profile.title);
-       allDoctors.push(docData.profile.first);
+       allDoctors.push(newDoctor);
        console.log(allDoctors.length);
      });
      if (allDoctors.length === 0) {
        $("#doctorOutput").text( "no doctors found!");
      } else {
   $("#doctorOutput").text(allDoctors.length + " doctors found!");
+  $("#show").toggleClass('hidden');
   }
 })
    .fail(function(error){
       console.log("fail");
   });
+    return allDoctors;
+};
+
+Doctor.prototype.displayDoctors = function () {
+allDoctors.forEach(function(doctor){
+  $("#listofDocs").append("<li>" + doctor.firstName + " " + doctor.lastName + " " + doctor.title);
+});
 };
 
 
