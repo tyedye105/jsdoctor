00068df48706cfe26a1b4d56eeea98e7ae04ede4
Marty <tyedye105@Martys-MacBook-Pro.local>
2017-01-06 16:58:58 -0800

    end of day commit. mvp.

--- js/doctor.js
+++ js/doctor.js
@@ -1,29 +1,25 @@
 var apiKey = require('./../.env').apiKey;
-allDoctors = []
+allDoctors = [];
 function Doctor(firstName, lastName, title) {
   this.firstName = lastName;
   this.lastName = firstName;
   this.title = title;
 }
 
-
-
-
-
 Doctor.prototype.getDoctors = function(medicalIssue) {
-  allDoctors = []
+  allDoctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(results) {
      results.data.forEach(function(docData){
        var newDoctor = new Doctor(docData.profile.first_name, docData.profile.last_name, docData.profile.title);
        allDoctors.push(newDoctor);
-       console.log(allDoctors.length);
      });
      if (allDoctors.length === 0) {
        $("#doctorOutput").text( "no doctors found!");
      } else {
-  $("#doctorOutput").text(allDoctors.length + " doctors found!");
+  $("#doctorOutput").text(allDoctors.length + " doctors found nearby!");
   $("#show").toggleClass('hidden');
+  console.log(results);
   }
 })
    .fail(function(error){
@@ -31,15 +27,11 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
   });
     return allDoctors;
 };
-
 Doctor.prototype.displayDoctors = function () {
 allDoctors.forEach(function(doctor){
   $("#listofDocs").append("<li>" + doctor.firstName + " " + doctor.lastName + " " + doctor.title);
-
 });
 };
-
-
 
 
 exports.doctorModule = Doctor;
